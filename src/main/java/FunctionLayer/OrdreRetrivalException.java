7160d7be53c65310454833e8935c0c100ee1aa33
BenjaminIglesias <cph-bc106@cphbusiness.dk>
2020-04-21 17:09:11 +0200

    Tilføjet en ordreRetrivalException til brug i en kommende ordremapper

---
+++ src/main/java/FunctionLayer/OrdreRetrivalException.java
@@ -0,0 +1,14 @@
+package FunctionLayer;
+
+/**
+ * The purpose of LoginSampleException is to...
+ * @author kasper
+ */
+public class OrdreRetrivalException extends Exception {
+
+    public OrdreRetrivalException(String msg) {
+        super(msg);
+    }
+
+
+}
