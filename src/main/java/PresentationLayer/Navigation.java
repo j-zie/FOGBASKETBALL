9c9b8cc21ca256289191204dfe927824e3574769
Jon Zielinski <notbald@protonmail.com>
2020-04-26 22:28:25 +0200

    kommentare til navigation

--- src/main/java/PresentationLayer/Navigation.java
+++ src/main/java/PresentationLayer/Navigation.java
@@ -8,27 +8,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
-
+/**
+ * Navigation klassen er en del af command mønstret
+ * Den modtager http requests og retunere strenge der repræsenere filstier til andre jsp pages.
+ */
 public class Navigation extends Command {
+    /**
+     * TODO(Jonathan) Til patrick: Hvis det er meningen du ikke vil have en default case, så skriv det som kommentar. Det er standard
+     * D
+     * Går til den jsp side som du giver som parametre i din request (kan være skjult.)
+     * @return Retunere en streng til en gyldig jsp side
+     * @param request fra jsp side med værdi "nav"
+     * @param response header informationer
+     **/
+
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
-
         HttpSession session = request.getSession();
         String nav = request.getParameter("Nav");
 
-
         switch (nav) {
-
             case "Katalog":
                 // Her kan der kaldes metode til evt. at hente alle carporte fra db.
                 return "Katalogpage";
             case "QuickByg":
-
-
                 if (session.getAttribute("user") == null) {
                     return "QuickByg/LoginOrSignuppage";
                 }
-
                 return "QuickByg/TagValgpage";
             case "login":
                 return "login";
@@ -36,9 +42,7 @@ public class Navigation extends Command {
                 return "Kurvpage";
             case "admin":
                 ArrayList<Ordre> alleOrdre = LogicFacade.hentAlleOrdre();
-
                 session.setAttribute("alleOrdre", alleOrdre);
-
                 return "AdminStyklisteRefpage";
             case "register":
                 return "register";
