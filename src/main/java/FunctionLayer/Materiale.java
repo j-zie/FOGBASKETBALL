ec138b9c5ee0ddfe1e3f4ef1810b98fc8ca0657d
BenjaminIglesias <cph-bc106@cphbusiness.dk>
2020-04-29 18:41:23 +0200

    Materiale objekt

---
+++ src/main/java/FunctionLayer/Materiale.java
@@ -0,0 +1,46 @@
+package FunctionLayer;
+
+public class Materiale {
+    private int materialeID;
+    private String materialeNavn;
+    private Double pris;
+
+    public Materiale(int materialeID, String materialeNavn, Double pris) {
+        this.materialeID = materialeID;
+        this.materialeNavn = materialeNavn;
+        this.pris = pris;
+    }
+
+    public int getMaterialeID() {
+        return materialeID;
+    }
+
+    public void setMaterialeID(int materialeID) {
+        this.materialeID = materialeID;
+    }
+
+    public String getMaterialeNavn() {
+        return materialeNavn;
+    }
+
+    public void setMaterialeNavn(String materialeNavn) {
+        this.materialeNavn = materialeNavn;
+    }
+
+    public Double getPris() {
+        return pris;
+    }
+
+    public void setPris(Double pris) {
+        this.pris = pris;
+    }
+
+    @Override
+    public String toString() {
+        return "Materiale{" +
+                "materialeID=" + materialeID +
+                ", materialeNavn='" + materialeNavn + '\'' +
+                ", pris=" + pris +
+                '}';
+    }
+}
