3a58b8cb4e737f0d3e9e3f37c71e724ffca4c3ba
Patrick Jahn <Patrick.jackobsen.jahn.109@gmail.com>
2020-05-03 20:23:26 +0200

    lille svg fix

--- src/main/java/SVG/SvgCarport.java
+++ src/main/java/SVG/SvgCarport.java
@@ -32,49 +32,6 @@ private void startSvg(int length, int width){
     SvgCarportText.append("<svg width=\"90%\" height=\"90%\" x=\"20\" y=\"20\" viewBox=\"0 0 "+length+ " "+ width+"\" preserveAspectRatio=\"xMinYMin\">" + "\n");
 
 }
-
-
-        private void drawPile(int length, int width){
-
-        // Selve pile hovederne
-         SvgCarportText.append("<defs>\n" +
-                 " <marker \n" +
-                 " \tid=\"beginArrow\" \n" +
-                 " \tmarkerWidth=\"12\" \n" +
-                 " \tmarkerHeight=\"12\" \n" +
-                 " \trefX=\"0\" \n" +
-                 " \trefY=\"6\" \n" +
-                 " \torient=\"auto\">\n" +
-                 "   <path d=\"M0,6 L12,0 L12,12 L0,6\" style=\"fill: #000000;\" />\n" +
-                 " </marker>\n" +
-                 " <marker \n" +
-                 " \tid=\"endArrow\" \n" +
-                 " \tmarkerWidth=\"12\" \n" +
-                 " \tmarkerHeight=\"12\" \n" +
-                 " \trefX=\"12\" \n" +
-                 " \trefY=\"6\" \n" +
-                 " \torient=\"auto\">\n" +
-                 "   <path d=\"M0,0 L12,6 L0,12 L0,0 \" style=\"fill: #000000;\" />\n" +
-                 " </marker>\n" +
-                 "</defs>\n");
-
-         // Selve pilene med hovederne på
-            // Lodrette pil
-         SvgCarportText.append("<line x1=\"5\"  y1=\"5\" x2=\"5\"  y2=\""+(width+30)+"\" \n" +
-                 "\tstyle=\"stroke: #006600;\n" +
-                 "\tmarker-start: url(#beginArrow);\n" +
-                 "\tmarker-end: url(#endArrow);\"/>");
-            // Vandrette pil
-            SvgCarportText.append("<line x1=\"14\"  y1=\"2\" x2=\""+(length+6+6)+"\"  y2=\"2\" \n" +
-                    "\tstyle=\"stroke: #006600;\n" +
-                    "\tmarker-start: url(#beginArrow);\n" +
-                    "\tmarker-end: url(#endArrow);\"/>");
-
-
-
-
-}
-
         private void EndSvg(){
         SvgCarportText.append("</svg>");
             SvgCarportText.append("</svg>");
