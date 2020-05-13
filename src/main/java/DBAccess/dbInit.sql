e62b16809a50bfcdf458f6804e2bd561a129d734
Jonathan Zielinski <jz@jonathans-mbp.home>
2020-05-13 11:45:13 +0200

    dbscript ændringer

--- src/main/java/DBAccess/dbInit.sql
+++ src/main/java/DBAccess/dbInit.sql
@@ -50,7 +50,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `fog_db`.`tagtyper` (
   `tagtypeNr` INT(11) NOT NULL AUTO_INCREMENT,
-  `tagtypeNavn` VARCHAR(45) NULL DEFAULT NULL,
+  `tagtypeNavn` VARCHAR(95) NULL DEFAULT NULL,
   `dækningsgrad` DOUBLE NULL DEFAULT NULL,
   PRIMARY KEY (`tagtypeNr`))
 
@@ -81,8 +81,28 @@ CREATE TABLE IF NOT EXISTS `fog_db`.`ordre` (
     FOREIGN KEY (`tagtypeNr`)
     REFERENCES `fog_db`.`tagtyper` (`tagtypeNr`)
     )
+    
 
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
+-- -----------------------------------------------------
+-- Table `fog_db`.`stykliste`
+-- -----------------------------------------------------
+
+CREATE TABLE `stykliste` (
+    `ordre` INT NOT NULL,
+    `materialer` INT NOT NULL,
+    `antal` INT NOT NULL,
+    PRIMARY KEY (`ordre`, `materialer`),
+	CONSTRAINT `Constr_materiale_fk`
+        FOREIGN KEY `materiale_fk` (`materialer`) REFERENCES `materialer` (`materialeID`)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `Constr_ordre_fk`
+        FOREIGN KEY `ordre_fk` (`ordre`) REFERENCES `fog_db`.`ordre` (`ordreNr`)
+        ON DELETE CASCADE ON UPDATE CASCADE
+)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -95,6 +115,7 @@ CREATE TABLE IF NOT EXISTS `fog_db`.`materialer` (
   `materialeID` INT(11) NOT NULL AUTO_INCREMENT,
   `materialeNavn` VARCHAR(100) NULL DEFAULT NULL,
   `materialeBeskrivelse` varchar(5000) NULL DEFAULT NULL,
+  `antal` INT NULL DEFAULT NULL, 
   `tag` VARCHAR(100) NULL DEFAULT NULL,
   `pris` DOUBLE NULL DEFAULT NULL,
   `path` VARCHAR(150) NULL DEFAULT NULL,
@@ -106,10 +127,8 @@ DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 
+-- noti
 
--- -----------------------------------------------------
--- Table `fog_db`.`notificationer`
--- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `fog_db`.`notificationer` (
   `notificationId` int(11) NOT NULL AUTO_INCREMENT,
     `brugerId` INT(11) NOT NULL,
@@ -121,32 +140,56 @@ CREATE TABLE IF NOT EXISTS `fog_db`.`notificationer` (
     REFERENCES `fog_db`.`bruger` (`brugerId`)
   )
 
+
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
-
 -- Some stuff to fill the pages
-insert into tagtyper (tagtypeNavn,dækningsgrad) values ("Plasttrapezplade-fladtag",0.35), ("Betontag-Rød",8.9) ,("Betontag-Sort", 8.9),("EternittagB6-Sort", 0.92),("EternittagB6-Rødbrun",0.92),("EternittagB7-Sort", 2.3),("EternittagB7-Mokka",2.3);
+insert into tagtyper (tagtypeNavn,dækningsgrad) values ("TRAPEZPLADE BLÅTONET 109X600CM PLASTMO ECOLITE TAGPLADE PVC",0.35),
+ ("Beton tegltagsten - Rød",8.9) ,("Beton tegltagsten - Sort	", 8.9),("EternittagB6 - Sort", 0.92),("EternittagB6 - Rødbrun",0.92),("EternittagB7 - Sort", 2.3),("EternittagB7 - Mokka",2.3);
 insert into bruger(email, password, rolle) values("admin", "admin", "Admin");
+insert into bruger(email, password, rolle) values("bruger", "bruger", "");
+
 insert into materialer(materialeNavn, materialeBeskrivelse, tag, pris ,path)
 values ("Trykimprægnerede stolpe 100x100mm", "Imprægneret stolpe til hegn, plankeværk eller andre
 		udendørs byggeprojekter der kræver modstandsdygtigt og slidstærkt træ.",
-		"stolpe", 76.99, "resources/Komponenter/StolpeTrykImp10x300cm.png"),
+		"default_stolpe", 76.99, "resources/Komponenter/StolpeTrykImp10x300cm.png"),
         ("Høvlet spærtræ 45x145 mm", "Høvlet spærtræ 45x145 mm trykimprægneret af fyr/gran anvendes
         til udendørs bærende konstruktioner. Spærtræet er derfor styrkesorteret og efterfølgende
         CE-mærket, så det er godkendt som konstruktionstræ. I daglig tale benævnes styrkesorteringen
         for spærtræ enten T1 eller K18. Trykimprægneret spærtræ bør ikke benyttes indendørs.",
-        "spær", 61.50, "resources/Komponenter/HoevletSpaerTrae45x145.png"),
+        "default_spær", 61.50, "resources/Komponenter/HoevletSpaerTrae45x145.png"),
         ("TRAPEZPLADE BLÅTONET 109X600CM PLASTMO ECOLITE TAGPLADE PVC", "Perfekt til udestuen og carporten. Blåtonet. 109 x 240 cm",
-        "bekældning", 250.00, "resources/Komponenter/blatonet-trapezplade-naturel-310-cm.jpg");
-
-insert into materialer(materialeNavn, pris, path, tag)
+        "default_bekældning", 250.00, "resources/Komponenter/blatonet-trapezplade-naturel-310-cm.jpg"),
+        ("Beton tegltagsten - Rød", "Den klassiske danske tegltagsten. Formen og den smukke røde farve er et billede på Danmark. 
+        Dantegl er et helt igennem dansk naturprodukt, der bygger på stolte håndværksmæssige traditioner, der går mere end 1.000 år tilbage i tiden.",
+        "bekældning", 72.85, "resources/Komponenter/beton-roed.jpg"),
+        ("Beton tegltagsten - Sort", "En moderne tegltagsten. Formen og den smukke sorte farve er et billede af verdens forfald. 
+        Dantegl er et helt igennem dansk naturprodukt, der bygger på stolte håndværksmæssige traditioner, der går mere end en milliard år tilbage i tiden.",
+        "bekældning", 72.85, "resources/Komponenter/beton-sort.jpg"),
+		("EternittagB6 - Sort", "Den klassiske danske tegltagsten. Formen og den smukke røde farve er et billede på Danmark. 
+        Dantegl er et helt igennem dansk naturprodukt, der bygger på stolte håndværksmæssige traditioner, der går mere end 1.000 år tilbage i tiden.",
+        "bekældning", 30.50, "resources/Komponenter/beton-sort.jpg"),
+		("EternittagB6 - Rødbrun", "Den klassiske danske tegltagsten. Formen og den smukke røde farve er et billede på Danmark. 
+        Dantegl er et helt igennem dansk naturprodukt, der bygger på stolte håndværksmæssige traditioner, der går mere end 1.000 år tilbage i tiden.",
+        "bekældning", 30.50, "resources/Komponenter/beton-roed.jpg"),
+		("EternittagB7 - Mokka", "Den klassiske danske tegltagsten. Formen og den smukke røde farve er et billede på Danmark. 
+        Dantegl er et helt igennem dansk naturprodukt, der bygger på stolte håndværksmæssige traditioner, der går mere end 1.000 år tilbage i tiden.",
+        "bekældning", 30.50, "resources/Komponenter/beton-sort.jpg");
+        
+insert into materialer(materialeNavn, pris, path, tag) 
 values("Carport 3,60X7,20M med redskabsrum 3,20X2,20M", 13447, "resources/Carporte/Carport1.png", "carport"),
 	("Carport enkelt 3,60X5,40M med Høj rejsning", 39213, "resources/Carporte/Carport2.png", "carport"),
 	("Carport 3,90X7,80M med redskabsrum 2,40X3,30M", 40000, "resources/Carporte/Carport3.png", "carport"),
 	("Carport 3,60X8,10M med redskabsrum 3,05X3,20M", 11111, "resources/Carporte/Carport4.png", "carport"),
 	("Carport 3,00X4,80M med Fladt tag", 19231, "resources/Carporte/Carport5.png", "carport");
 
+insert into materialer(materialeNavn, pris, path, tag) 
+values("Plasttrapezplade-fladtag", 13447, "resources/Carporte/Carport1.png", "carport"),
+	("Carport enkelt 3,60X5,40M med Høj rejsning", 39213, "resources/Carporte/Carport2.png", "carport"),
+	("Carport 3,90X7,80M med redskabsrum 2,40X3,30M", 40000, "resources/Carporte/Carport3.png", "carport"),
+	("Carport 3,60X8,10M med redskabsrum 3,05X3,20M", 11111, "resources/Carporte/Carport4.png", "carport"),
+	("Carport 3,00X4,80M med Fladt tag", 19231, "resources/Carporte/Carport5.png", "carport");
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
