9406e1fc0ac41becca87aaccfc5d5839ed13aedb
Jonathan Zielinski <jz@jonathans-mbp.home>
2020-05-16 23:59:04 +0200

    karport alignment

--- src/main/java/FunctionLayer/Formatter.java
+++ src/main/java/FunctionLayer/Formatter.java
@@ -7,14 +7,12 @@ public class Formatter {
      * Bliver brugt til at fixerere katalog paragraf
      */
     public static String fixedStrLength(String aStr) {
-        if (aStr.length() > 30) {
-            return aStr.substring(0,30) + " ...";
-        } else {
-            String spaces = "";
-            for (int i = 0; i < ((30 + 4) - aStr.length()); i++) {
-                spaces += " ";
-            }
-            return aStr + spaces;
+        int lineLength = 22;
+        if (aStr.length() > lineLength) {
+            return aStr.substring(0, 19) + " ...";
+        }
+        else {
+            return aStr; // empty line
         }
     }
 }
